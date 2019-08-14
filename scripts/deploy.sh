19efb7cc15f3c685693a0362b246a40b5aaaac8b
Márton Elek <elek@apache.org>
2019-08-14 21:13:02 +0200

    k8s deployment script

---
+++ scripts/deploy.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -x
+
+NAMESPACE=ozone-daily
+
+echo "Delete existing resources"
+kubectl delete statefulset --all --namespace $NAMESPACE
+kubectl delete daemonset --all --namespace $NAMESPACE
+kubectl delete deployment --all --namespace $NAMESPACE
+kubectl delete service --all --namespace $NAMESPACE
+kubectl delete configmap --all --namespace $NAMESPACE
+kubectl delete secret --all --namespace $NAMESPACE
+kubectl delete pod --all --namespace $NAMESPACE
+
+sleep 60
+
+#install flekszible
+sudo wget https://os.anzix.net/flekszible -O /usr/local/bin/flekszible
+sudo chmod +x /usr/local/bin/flekszible
+
+cd /workdir/examples/ozone-dev/
+flekszible generate --namespace $NAMESPACE
+kubectl apply -f
+sleep 100000
