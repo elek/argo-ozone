85203183e34726304ecf1a618e457e6ca6017845
Márton Elek <elek@apache.org>
2019-08-24 21:15:53 +0200

    use configuration from the git repo + date prefix

--- build-klepif.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -e
-docker build --no-cache -f docker-ozone-klepif/Dockerfile . -t elek/ozone-klepif
-docker push elek/ozone-klepif
--- byscane-nightly.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-argo submit -p notify=true -p branch=ozone-0.4.1 ozone-build.yaml --generate-name byscane-nightly-
